8a1b780b9e898929a46aa68e537c203cfae2786a
hujiafeng <962538375@qq.com>
2017-12-29 11:44:23 +0800

    no message

--- apit/pay/wxpay/callback.php
+++
@@ -1,8 +0,0 @@
-<?php
-header ( 'Content-type: application/json' );
-require 'TestpayAction.class.php';
-$input = new TestpayAction();
-
-$order = $input->callback();
-
-echo json_encode($order);
--- apit/pay/wxpay/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header ( 'Content-type: application/json' );
-require 'TestpayAction.class.php';
-
-$input = new TestpayAction();
-//微信支付
-$order = $input->wxpay($_POST['no'],$_POST['reward']);
-echo json_encode($order);
